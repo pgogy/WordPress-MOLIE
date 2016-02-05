3fffe00bc964a6e8d61a66de41f224421321f798
patlockley <patrick.lockley@googlemail.com>
2016-02-05 18:29:52 +0000

    Added in new classes

--- molie.php
+++ molie.php
@@ -37,11 +37,23 @@
 	
 	$MOLIE = new MOLIE();
 	
-	require_once("classes/molie-course-ajax.php");
-	require_once("classes/molie-choose-ajax.php");
+	require_once("classes/ajax/molie-course-ajax.php");
+	require_once("classes/ajax/molie-file-ajax.php");
+	require_once("classes/ajax/molie-quiz-ajax.php");
+	require_once("classes/ajax/molie-assignment-ajax.php");
+	require_once("classes/ajax/molie-choose-ajax.php");
+	require_once("classes/ajax/molie-discussion-ajax.php");
+	require_once("classes/post_types/molie-course-post.php");
+	require_once("classes/post_types/molie-quiz-post.php");
+	require_once("classes/post_types/molie-discussion-post.php");
+	require_once("classes/post_types/molie-quiz-answer-post.php");
 	require_once("classes/molie-admin-roles.php");
-	require_once("classes/molie-custom-post.php");
 	require_once("classes/molie-help.php");
 	require_once("classes/molie-link.php");
 	require_once("classes/molie-choose.php");
+	require_once("classes/molie-files.php");
 	require_once("classes/molie-quiz.php");
+	require_once("classes/molie-discussion.php");
+	require_once("classes/molie-assignments.php");
+	require_once("classes/molie-guide.php");
+	require_once("classes/molie-menu.php");

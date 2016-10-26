7afaa0bbfd3fe348c2a5e5422e35adb1c8136b38
patlockley <patrick.lockley@googlemail.com>
2016-10-26 14:01:04 +0100

    Made sure get_current_screen is callable

--- classes/post_types_editor/molie-hide-roster-post.php
+++ classes/post_types_editor/molie-hide-roster-post.php
@@ -9,14 +9,16 @@
 		function hide_roster($query){
 			if(is_admin()){
 				$screen = get_current_screen();
-				if($screen->base=="edit"){
-					global $wpdb;
-					$rosters = array();
-					$data = $wpdb->get_results("select * from " . $wpdb->prefix . "postmeta where meta_key = 'CanvasRoster'");
-					foreach($data as $roster){
-						array_push($rosters, $roster->post_id);
+				if(is_callable("get_current_screen")){
+					if($screen->base=="edit"){
+						global $wpdb;
+						$rosters = array();
+						$data = $wpdb->get_results("select * from " . $wpdb->prefix . "postmeta where meta_key = 'CanvasRoster'");
+						foreach($data as $roster){
+							array_push($rosters, $roster->post_id);
+						}
+						$query->query_vars['post__not_in'] = array_merge($query->query_vars['post__not_in'], $rosters);
 					}
-					$query->query_vars['post__not_in'] = array_merge($query->query_vars['post__not_in'], $rosters);
 				}
 			}
 		}	
